a35b8c2aaa0f05db9ed118d0aad794cd490d8b61
sebastianbarkan <86000650+sebastianbarkan@users.noreply.github.com>
2023-02-11 14:21:31 -0500

    complete landing page main functionalities

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const inputContainer = document.querySelector(".main-input-container");
+
+function SendMail() {
+  var templateParams = {
+    sender_email: document.getElementById("senderEmail").value,
+  };
+
+  console.log(templateParams);
+
+  emailjs.send("service_c1r5od5", "template_2ovtlqp", templateParams).then(
+    function (response) {
+      inputContainer.style.boxShadow = "none";
+      inputContainer.style.background = "none";
+      inputContainer.innerHTML = `<p class="success-message"><span class="success-span">Congratulations!</span> You will be notified when we launch!</p>`;
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+}
